f4fc443ce3f21d70b6e800ffc7972ee91bcab793
Keryann Le Besque <keryannlebesque@gmail.com>
2025-12-09 12:00:08 +0100

    Modification AffichageCui

--- Partie2/src/vue/AffichageCUI.java
+++ Partie2/src/vue/AffichageCUI.java
@@ -1,21 +1,61 @@
 package vue;
 
+
 import java.util.ArrayList;
 
 import metier.classe.Classe;
+import metier.classe.Methode;
+import metier.classe.Attribut;
+import metier.classe.Parametre;
 
 public class AffichageCUI 
 {
 	
-	public void afficherClasse (ArrayList<Classe> lstClasse)
-	{
+	public String afficherClasse (ArrayList<Classe> lstClasse)
+	{	
+		final String LIGNE = "------------------------------------------------";
+		String sRet="";
 		for (Classe classe : lstClasse) 
 		{
-			System.out.println("------------------------------------------------");
-			System.out.println(String.format("%48s", classe.getNom()));
-			System.out.println("------------------------------------------------");
-			if ( )
+			sRet += LIGNE + "\n";
+			sRet += String.format("%48s", classe.getNom()) + "\n";
+			sRet += LIGNE + "\n";
+
+			for ( Attribut att : classe.getLstAttribut() )
+			{
+				if ( att.constante() )
+				{
+					sRet += att.visibilite() + " " + att.nom() + " :" + att.type()+ "\n";
+				}
+			}
+			if ( classe.getNbConstante() > 0 )
+				sRet += LIGNE + "\n";
+
+			for ( Attribut att : classe.getLstAttribut() )
+			{
+				if ( !att.constante() )
+				{
+					sRet += att.visibilite() + " " + att.nom() + " :" + att.type() + "\n";
+				}
+			}
+			sRet += LIGNE + "\n";
+
+			for ( Methode meth : classe.getLstMethode() )
+			{
+				sRet += meth.visibilite() + " " + meth.methode() + "(";
+				for ( Parametre p : meth.lstParam() )
+				{
+					sRet += p.nom() + ":" + p.type() + ",";
+				}
+				if ( meth.lstParam().size() > 0 )
+					sRet = sRet.substring(0, sRet.length()-1);
+				sRet += ")" + "\t" + ":" + meth.type() + "\n";				
+			}	
+			
 		}
+		sRet += LIGNE;
+
+		return sRet;
 		
 	}
 }	
