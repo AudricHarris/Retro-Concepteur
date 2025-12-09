ff216fe0b2e3318d6a915ef95b24aee3ef76e5df
Keryann Le Besque <keryannlebesque@gmail.com>
2025-12-09 09:15:55 +0100

    Amelioration du toString

--- Partie1/src/metier/classe/Methode.java
+++ Partie1/src/metier/classe/Methode.java
@@ -12,16 +12,16 @@ public record Methode(String visibilite,String methode, String type, ArrayList<P
     public String toString()
     {
         String sRet ="";
-        sRet += "methode : " + String.format("%20s",this.methode) + "	visibilité: " ;
-        sRet += String.format("%10s", this.visibilite ) + " type de retour: ";
-        sRet += String.format("%10s", this.type) + "\nparamètres : \n";
+        sRet += "methode : " + String.format("%-20s",this.methode) + "	visibilité: " ;
+        sRet += String.format("%-10s", this.visibilite ) + " type de retour: ";
+        sRet += String.format("%-10s", this.type) + "\nparamètres : \n";
 
 		int cpt=0;
         if ( this.lstParam.isEmpty() )
             sRet += "aucun\n";
         else
             for ( Parametre param : this.lstParam )
-                sRet += "	p" + (++cpt) + ":" + String.format("%10s", param.nom()) + " type:" + param.type() + "\n ";
+                sRet += "	p" + (++cpt) + ": " + String.format("%-10s", param.nom()) + " type:" + param.type() + "\n ";
         
 		return sRet;
     }
