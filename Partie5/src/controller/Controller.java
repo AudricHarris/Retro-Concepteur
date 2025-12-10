e21dd31d8c1390a319acf04d2ddb22016a2f302f
Keryann Le Besque <keryannlebesque@gmail.com>
2025-12-10 10:30:48 +0100

    Ajout du début de la partie 5

---
+++ Partie5/src/controller/Controller.java
@@ -0,0 +1,30 @@
+package controller;
+
+
+import metier.AnalyseFichier;
+import vue.FrameUML;
+
+/*
+ * Controller est la pont entre notre logique et l'IHM
+ * TODO : Ammeliorer le système de chemin de fichier pour eviter de le changer à chaque fois
+ */
+public class Controller 
+{
+	private AnalyseFichier analyseFichier;
+	private FrameUML   frame;
+
+
+	public Controller() 
+	{
+		this.analyseFichier = new AnalyseFichier("/home/etudiant/lk240510/Documents/TESTSAE301");
+		this.frame = new FrameUML();
+
+	}
+
+
+	public static void main(String[] args) 
+	{
+		new Controller();
+	}
+
+}
---
+++ Partie5/src/vue/FrameUML.java
@@ -0,0 +1,18 @@
+package vue;
+
+import javax.swing.*;
+
+public class FrameUML extends JFrame
+{
+    public FrameUML()
+    {
+        this.setLocation(500, 500);
+        this.setSize(500, 500);
+        this.setTitle("Diagramme UML");
+
+        
+
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+}
