a433b4afbd5d9ff9e501560a85f64625cc1b3076
Lucas <lucasleprevost9@gmail.com>
2025-12-12 13:56:22 +0100

    souligne

--- Partie3/run.bat
+++ Partie3/run.bat
@@ -1,18 +1,14 @@
 @echo off
-REM 
-chcp 65001 > nul
 
 echo Compilation du projet RetroConcepteur...
-REM 
-javac -encoding UTF-8 "@Compile.list" -d ./class
+javac "@Compile.list" -d ./class
 
-REM 
+REM Vérifier si la compilation a réussi
 if %ERRORLEVEL% EQU 0 (
     echo Exécution du programme...
     echo.
-    REM 
-    REM 
-    java -Dfile.encoding=UTF-8 -cp ./class RetroConcepteur.Controller %1
+    REM Passer le répertoire data en argument
+    java -cp ./class RetroConcepteur.Controller %1
 ) else (
     echo Erreur de compilation!
     exit /b 1
